051a83753fb0020f563dd567d105e7be00dcfb54
Ayam62 <ayamkattel62@gmail.com>
2025-01-08 10:21:44 +0545

    bug of mungodb connection is fixed- bug was in the cloud

--- DummyData/main.js
+++ DummyData/main.js
@@ -3,26 +3,43 @@ import mongoose from "mongoose";
 const app = express();
 import Employee from "./models/employee.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+//connection to mongodb
+mongoose.connect("mongodb+srv://ayam:ayam@cluster0.xdvx3nz.mongodb.net/")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
+
 
 const port = 3000;
 
+//connection to my ejs/html
 app.set("view engine", "ejs");
 
+//rendering html/ejs for ui
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+//in generate route do the following. note down that it is an async funciton because we have to create database so..
 app.get("/generate", async (req, res) => {
-  //generate random data
+
+  //generate random data inside the loop
+  const names=["Ayam","Asmit","Sujal","Dipin","Utsab"]
+  const salary=[45000, 78000, 62000, 33000, 97000]
+  const language=["Python", "JavaScript", "Go", "Ruby", "C++"]
+  const cities = ["Kathmandu", "Pokhara", "Biratnagar", "Damak", "Chitwan"];
+  const isManager=[true,false,true,false,true]
+
+  const random=Math.floor(Math.random()*5)
+
   for (let index = 0; index < 10; index++) {
     let e =await Employee.create({
-      name: "Ayam",
-      salary: 345,
-      language: "Python",
-      city: "damak",
-      isManager: true,
+      name: names[random],
+      salary: salary[random],
+      language: language[random],
+      city: cities[random],
+      isManager: isManager[random],
     });
-    console.log(e)
+    
   }
   res.render("index");
 });
