ba5b2a6bbb4f033f3b103e32f4c1ab0030d63323
Ayam62 <ayamkattel62@gmail.com>
2025-01-14 12:56:56 +0545

    todo-fullstack successfully completed

--- To-Do_FullStack/To-Do/Server/server.js
+++ To-Do_FullStack/To-Do/Server/server.js
@@ -32,8 +32,14 @@ app.post("/add",(req,res)=>{
 })
 
 app.put("/update/:id",(req,res)=>{
-    const id=req.params//always use params to extract id..more specifically  when retreiving any data from url itself after colon
+    const {id}=req.params//always use params to extract id..more specifically  when retreiving any data from url itself after colon
     TodoModel.findByIdAndUpdate({_id:id},{isCompleted:true})//if this id matches with database's id then update isCompleted to true
+    .then(result=>res.json(result))
+    .catch(err=>res.json(err))
+})
+app.delete("/delete/:id",(req,res)=>{
+    const {id}=req.params//always use params to extract id..more specifically  when retreiving any data from url itself after colon
+    TodoModel.findByIdAndDelete({_id:id})//if this id matches with database's id then update isCompleted to true
     .then(result=>res.json(result))
     .catch(err=>res.json(err))
 })
--- To-Do_FullStack/To-Do/src/components/Home.jsx
+++ To-Do_FullStack/To-Do/src/components/Home.jsx
@@ -1,24 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
-  const[task,setTask]=useState("")
-  const handleAdd=()=>{
-    console.log(task)
-    axios.post('http://localhost:3000/add', {task:task})
-    .then(response => console.log(response.data))
-    .catch(error => console.error(error));
-  }
+  const [task, setTask] = useState("");
 
-  useEffect(()=>{ //to load all task immediately after refresing page or in the starting of page
-    axios.get('http://localhost:3000/get')
-    .then(result=> setTodos(result.data))//receives all data from the result(server)
-    .catch(err=>console.log(err))
-  },[])
+  const handleAdd = () => {
+    console.log(task);
+    if (task) {
+      axios
+        .post("http://localhost:3000/add", { task: task })
+        .then((response) => console.log(response.data))
+        .catch((error) => console.error(error));
+    }
+  };
+  const handleDelete = (id) => {
+    axios
+    .delete("http://localhost:3000/delete/" + id)
+    .then((result) => console.log(result)) //receives all data from the result(server)
+    .catch((err) => console.log(err));
+  };
 
-  
+  const handleEdit = (id) => {
+    axios
+      .put("http://localhost:3000/update/" + id)
+      .then((result) => console.log(result)) //receives all data from the result(server)
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    //to load all task immediately after refresing page or in the starting of page
+    axios
+      .get("http://localhost:3000/get")
+      .then((result) => setTodos(result.data)) //receives all data from the result(server)
+      .catch((err) => console.log(err));
+  }, [todos]);
+
   return (
     <div className="Home w-full bg-gray-700 h-screen flex justify-center items-center">
       <div className="container bg-slate-800">
@@ -32,44 +50,36 @@ const Home = () => {
               placeholder="Enter your task here"
               onChange={(e) => setTask(e.target.value)}
             />
-            <button className="button-add" onClick={handleAdd}>Add</button>
+            <button className="button-add" onClick={handleAdd}>
+              Add
+            </button>
           </div>
-          <div className="todo-task">
-            <div className="front-part">
-              <input type="checkbox" />
-              <div className="to-do-content">
-                <p>Actual task</p>
-              </div>
+          {todos.length === 0 ? (
+            <div className="NoTask">
+              <h2>Hurray. You have no task todayyy</h2>
             </div>
-            <button className="button-delete ">Delete</button>
-          </div>
-          
-          {
-        todos.length===0?
-        <div className="NoTask">
-            <h2>Hurray. You have no task todayyy</h2>
-        </div>
-          :todos.map(todo=>(
-            <div className="todo-task">
-            <div className="front-part">
-              <input type="checkbox" />
-              <div className="to-do-content">
-                <p>{todo.task}</p>
+          ) : (
+            todos.map((todo) => (
+              <div className="todo-task">
+                <div
+                  className="front-part"
+                  onClick={() => handleEdit(todo._id)}
+                >
+                  <div className="to-do-content">
+                    <p className={todo.isCompleted ? "line-through" : "todo-style"}>
+                      {todo.task}
+                    </p>
+                  </div>
+                </div>
+                <button
+                  className="button-delete "
+                  onClick={() => handleDelete(todo._id)}
+                >
+                  Delete
+                </button>
               </div>
-            </div>
-            <button className="button-delete ">Delete</button>
-          </div>
-          ))}
-          
-          <div className="todo-task">
-            <div className="front-part">
-              <input type="checkbox" />
-              <div className="to-do-content">
-                <p>Actual task</p>
-              </div>
-            </div>
-            <button className="button-delete ">Delete</button>
-          </div>
+            ))
+          )}
         </div>
       </div>
     </div>
