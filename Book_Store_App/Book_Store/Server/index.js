61214b0193066fceaa3fa27bd85c92e4cd5a5c17
Ayam62 <ayamkattel62@gmail.com>
2025-01-20 22:32:48 +0545

    collection made

--- Book_Store_App/Book_Store/Server/index.js
+++ Book_Store_App/Book_Store/Server/index.js
@@ -29,6 +29,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    //create a collection for the database
+
+    const books=client.db("BookInventory").collection("books")
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
